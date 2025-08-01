3dfc2a23646dd7c968c61852fc5450515d36c12a
Himansi-dudhat <139256462+Himansi-dudhat@users.noreply.github.com>
2025-08-01 12:24:45 +0530

    fix:removed undefinedFunction causin crash,fixed #1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log("Script is loaded and working!");
 function submitTest() {
-    // Bug: `undefinedFunction()` is not defined, which causes a crash
     alert("Submitting your test...");
-    undefinedFunction();  // This will crash the site!
+    console.log("Test submitted successfully.");
 }
